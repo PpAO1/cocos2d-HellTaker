637bc76162a6ffb6b5d3786a5b17e102a64a05fd
정재환 <cc4036@naver.com>
2023-04-03 20:50:11 +0900

    23 04 03 플레이어 애니메이션 거의 완성..

--- Classes/Player.cpp
+++ Classes/Player.cpp
@@ -1,11 +1,13 @@
 #include "Player.h"
 
 USING_NS_CC;
+using namespace std;
 
 Player* Player::_instance = nullptr;
 
 Player::Player()
 {
+	
 }
 
 Player::~Player()
@@ -29,12 +31,14 @@ bool Player::init()
 	pPlayer = Sprite::create("Sprite/hero0022.png");
 	this->setContentSize(pPlayer->getContentSize());
 	this->addChild(pPlayer);
-	this->PlayerIdleAnim();
+	scheduleOnce(schedule_selector(Player::PlayerIdleAnim), 0.01f);
 	return true;
 }
 
-void Player::PlayerIdleAnim()
+void Player::PlayerIdleAnim(float f)
 {
+	pPlayer->stopAllActions();
+
 	auto animation = Animation::create();
 
 	animation->setDelayPerUnit(0.07f);
@@ -52,6 +56,134 @@ void Player::PlayerIdleAnim()
 	pPlayer->runAction(rep);
 }
 
+void Player::PlayerMoveAnim()
+{
+	pPlayer->stopAllActions();
+
+	auto animation = Animation::create();
+
+	animation->setDelayPerUnit(0.07f);
+
+	char str[100] = { 0, };
+
+	for (int i = 3; i < 9; i++)
+	{
+		sprintf(str, "Sprite/assets100V2005%d.png", i);
+		animation->addSpriteFrameWithFile(str);
+	}
+
+	auto animate = Animate::create(animation);
+	pPlayer->runAction(animate);
+	scheduleOnce(schedule_selector(Player::PlayerIdleAnim), 0.42f);
+}
+
+void Player::PlayerHitAnim()
+{
+	pPlayer->stopAllActions();
+
+	auto animation = Animation::create();
+
+	animation->setDelayPerUnit(0.07f);
+
+	char str[100] = { 0, };
+
+	for (int i = 40; i < 53; i++)
+	{
+		sprintf(str, "Sprite/hero00%d.png", i);
+		animation->addSpriteFrameWithFile(str);
+	}
+
+	auto animate = Animate::create(animation);
+	pPlayer->runAction(animate);
+	scheduleOnce(schedule_selector(Player::PlayerIdleAnim), 0.84f);
+}
+
+void Player::PlayerDamagedAnim()
+{
+	auto huge = Sprite::create("Sprite/huge_vfx0022.png");
+	huge->setPosition(pPlayer->getPosition());
+	huge->setZOrder(10);
+	this->addChild(huge);
+
+	auto animation = Animation::create();
+
+	animation->setDelayPerUnit(0.07f);
+
+	char str[100] = { 0, };
+
+	for (int i = 22; i < 28; i++)
+	{
+		sprintf(str, "Sprite/huge_vfx00%d.png", i);
+		animation->addSpriteFrameWithFile(str);
+	}
+
+	auto animate = Animate::create(animation);
+	huge->runAction(animate);
+
+	auto blinkAnim = Blink::create(1.0f, 4);
+	pPlayer->runAction(blinkAnim);
+}
+
+
+
 void Player::update(float f)
 {
+
+}
+
+void Player::onKeyPressed(cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event* event)
+{
+	switch (keycode)
+	{
+	case EventKeyboard::KeyCode::KEY_ENTER:
+		this->PlayerMoveAnim();
+		break;
+
+	case EventKeyboard::KeyCode::KEY_UP_ARROW:
+		this->PlayerHitAnim();
+		break;
+
+	case EventKeyboard::KeyCode::KEY_DOWN_ARROW:
+		this->PlayerDamagedAnim();
+		break;
+
+	case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
+		break;
+
+	case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:
+		break;
+	}
+}
+
+void Player::onKeyReleased(cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event* event)
+{
+	switch (keycode)
+	{
+	case EventKeyboard::KeyCode::KEY_ENTER:
+		break;
+
+	case EventKeyboard::KeyCode::KEY_UP_ARROW:
+		break;
+
+	case EventKeyboard::KeyCode::KEY_DOWN_ARROW:
+		break;
+	}
+}
+
+void Player::onEnter()
+{
+	Layer::onEnter();
+
+	_listener = EventListenerKeyboard::create();
+
+	_listener->onKeyPressed = CC_CALLBACK_2(Player::onKeyPressed, this);
+	_listener->onKeyReleased = CC_CALLBACK_2(Player::onKeyReleased, this);
+
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(_listener, this);
+}
+
+void Player::onExit()
+{
+	_eventDispatcher->removeEventListener(_listener);
+	Layer::onExit();
 }
--- Classes/Player.h
+++ Classes/Player.h
@@ -7,12 +7,21 @@ private:
 	Player ();
 	~Player ();
 	static Player* _instance;
+	cocos2d::EventListenerKeyboard* _listener;
 
 public:
 	cocos2d::Sprite* pPlayer;
 	static Player& getInstance();
 	virtual bool init();
 	
-	void PlayerIdleAnim();
+	void PlayerIdleAnim(float f);
+	void PlayerMoveAnim();
+	void PlayerHitAnim();
+	void PlayerDamagedAnim();
 	void update(float f);
+
+	void onKeyPressed(cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event* event);
+	void onKeyReleased(cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event* event);
+	void onEnter();
+	void onExit();
 };
